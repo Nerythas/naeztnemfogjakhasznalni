bf431df5dd3b69e214f4498f035a31171bec1019
Nerythas <thesurm@gmail.com>
2026-09-17 22:50:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 export const SUPABASE_URL = "https://szbghczunegxfwntbqce.supabase.co";
-export const SUPABASE_ANON_KEY = NEXT_PUBLIC_SUPABASE_URL=https://szbghczunegxfwntbqce.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_Z68HM6MTjLhL36HrWldBhw_LaKYuWcY
+
+export const SUPABASE_ANON_KEY = "sb_publishable_Z68HM6MTjLhL36HrWldBhw_LaKYuWcY";
