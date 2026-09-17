b25262d3b96fb1329c264dd935b73b52490bc672
Nerythas <thesurm@gmail.com>
2026-09-17 22:36:25 +0200

    Change Supabase URL and anon key
    Updated Supabase project credentials in config.js.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// IDE írd be a saját Supabase projekted adatait.
-// Supabase Dashboard → Project Settings → API
-export const SUPABASE_URL = "https://SAJAT-PROJEKT.supabase.co";
-export const SUPABASE_ANON_KEY = "IDE_JON_A_PUBLISHABLE_ANON_KEY";
+
+export const SUPABASE_URL = "https://szbghczunegxfwntbqce.supabase.co";
+export const SUPABASE_ANON_KEY = NEXT_PUBLIC_SUPABASE_URL=https://szbghczunegxfwntbqce.supabase.co
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_Z68HM6MTjLhL36HrWldBhw_LaKYuWcY
